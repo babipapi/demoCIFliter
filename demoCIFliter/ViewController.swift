32ad8154186a92bf621ea9dfd166a213288d263e
Daniel <babipapi@gmail.com>
2020-03-18 23:05:58 +0800

    fix

--- demoCIFliter/ViewController.swift
+++ demoCIFliter/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import CoreImage.CIFilterBuiltins
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var colorChangeChoose: UISegmentedControl!
+    @IBOutlet weak var picImageView: UIImageView!
+    @IBAction func filterSliderChange(_ sender: UISlider) {
+        if colorChangeChoose.selectedSegmentIndex == 0 {
+            let image = UIImage(named: "a")
+            let ciImage = CIImage(image: image!)
+            let filter = CIFilter.colorMonochrome()
+            filter.inputImage = ciImage
+            filter.intensity = sender.value
+            if let outputCIImage = filter.outputImage{
+                let filterImage = UIImage(ciImage: outputCIImage)
+                picImageView.image = filterImage
+            }
+        } else {
+            let image = UIImage(named: "a")
+            let ciImage = CIImage(image: image!)
+            let filter = CIFilter.colorControls()
+            filter.inputImage = ciImage
+            filter.brightness = sender.value
+            if let outputCIImage = filter.outputImage{
+                let filterImage = UIImage(ciImage: outputCIImage)
+                picImageView.image = filterImage
+            }
+        }
     }
-
-
+    
 }
 
